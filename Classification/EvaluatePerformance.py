9540a429576fe00dd3fe598c7a8e471fb0375a7f
Zainab Aldhanhani <101204152+ZainabAldhanhani@users.noreply.github.com>
2024-11-14 17:26:09 +0400

    Add files via upload

---
+++ Classification/EvaluatePerformance.py
@@ -0,0 +1,28 @@
+
+from sklearn.metrics import accuracy_score, f1_score,confusion_matrix,classification_report
+import seaborn as sns
+import matplotlib.pyplot as plt
+ # Set backend to TkAgg
+def Evaluation_report(y_test,y_pred):
+    target_names=['normal', 'adenocarcinoma', 'large.cell.carcinoma', 'squamous.cell.carcinoma']
+    accuracy = accuracy_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred, average='weighted')
+    report = classification_report(y_test, y_pred, target_names=target_names)
+    # Print results
+    print(f"\nTest Accuracy: {accuracy:.4f}")
+    print(f"Test F1 Score: {f1:.4f}")
+    print("\nClassification Report:")
+    print(report)
+
+def Evaluation_confusion_matrix(y_test,y_pred):
+    # Calculate confusion matrix
+    conf_matrix = confusion_matrix(y_test, y_pred)
+    # Plot confusion matrix as a heatmap
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues',
+                xticklabels=['Normal', 'Adenocarcinoma', 'Large Cell Carcinoma', 'Squamous Cell Carcinoma'],
+                yticklabels=['Normal', 'Adenocarcinoma', 'Large Cell Carcinoma', 'Squamous Cell Carcinoma'])
+    plt.xlabel('Predicted Labels')
+    plt.ylabel('True Labels')
+    plt.title('Confusion Matrix')
+    plt.show()
